5df7dd13bfb15de4ceeac8a39deb4dda80cfb990
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-08-02 15:02:04 -0700

    Finished

--- solution.py
+++ solution.py
@@ -1,40 +1,20 @@
-# def isSubsequence(s, t):
-#     ascending = []
-#     for char in s:
-#         map(t.find, char)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-# def isSubsequence(s, t):
-#     ascending = list(map(t.find, s))
-#     print(ascending)
-#     return False if -1 in ascending else sorted(ascending) == ascending and len(set(ascending)) == len(ascending)
 
-def isSubsequence1(s, t): # 5% cop
-    if not s:
-        return True
-    if not t:
-        return False
-    if s[0] == t[0]:
-        return isSubsequence(s[1:], t[1:])
-    return isSubsequence(s, t[1:])
-
-def isSubsequence2(s, t): # 74% 
-    t = iter(t)
-    return all(c in t for c in s)
-
-def isSubsequence(s, t): # 83%
-    for c in s:
-        i = t.find(c)
-        if i == -1:
-            return False
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    true_list = ListNode(0)
+    current = true_list
+    while list1 and list2:
+        if list1.val > list2.val:
+            current.next = list2 # this is .next and not .val because .next is a List while val is just a value.
+            list2 = list2.next
         else:
-            t = t[i+1:]
-    return True
-
-print(isSubsequence('abc', 'abcdabdab')) # True
-print(isSubsequence('abc', 'ahbgdc')) # True
-print(isSubsequence('axc', 'ahbgdc')) # False
-print(isSubsequence('', 'ahbgdc')) # True
-print(isSubsequence('abc', '')) # False
-print(isSubsequence('b', 'c')) # False
-print(isSubsequence("aaaaaa", "bbaaaa")) # False
-print(isSubsequence("ab", "baab")) # True
+            current.next = list1
+            list1 = list1.next
+        current = current.next
+    current.next = list1 or list2
+    return true_list.next
