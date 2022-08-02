df03cd2c49c11740b232218ac8103608ee86fb3b
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-08-02 15:21:31 -0700

    added recursive solution

--- solution.py
+++ solution.py
@@ -18,3 +18,20 @@ def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optio
         current = current.next
     current.next = list1 or list2
     return true_list.next
+
+
+# recursive solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+def mergeTwoLists2( list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    if list1 == None: return list2
+    if list2 == None: return list1
+    if list1.val < list2.val:
+        list1.next = mergeTwoLists2(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists2(list1, list2.next)
+        return list2
